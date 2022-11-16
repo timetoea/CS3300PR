9063d4083de0d1183a3bf2c6442caa1cadf124cb
tarnoldf <timoteo.david10@gmail.com>
2022-11-16 21:48:07 +0000

    Added Singup

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.0.2"
 
+gem "devise"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3"
 
@@ -78,4 +80,10 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem 'simplecov'
+end
+
+
+group :development, :test do
+  gem 'capybara'
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -7,9 +7,29 @@ RSpec.describe Project, type: :model do
     end
     it "should be able to save project" do
       project = Project.new(title: "Title", description: "Some description content goes here")
-      expect(project.save?).to eq(true)
+      expect(project.save).to eq(true)
     end
   end
   context "scopes tests" do
+  end
+end
+
+require "rails_helper"
+
+RSpec.describe Project, type: :model do
+# ...
+
+context "scopes tests" do
+     let(:params) { { title: "Title", description: "some description" } }
+     before(:each) do
+        Project.create(params)
+        Project.create(params)
+        Project.create(params)
+     end
+
+     it "should return all projects" do
+        expect(Project.count).to eq(3)
+     end
+     
   end
 end
